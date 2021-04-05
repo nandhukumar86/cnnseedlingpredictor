06fa4331757079d61171ced3a7683d50968c6e0d
Nandhakumar <nandhu_kumar86@yahoo.com>
2021-04-06 00:03:34 +0530

    changes in node

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cnnapp1'));
+app.use(express.static('./dist/cnnapp1'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/cnnapp1/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/cnnapp1/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
