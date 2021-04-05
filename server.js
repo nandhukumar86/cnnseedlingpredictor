4b07b7d8b76324310047f54e5918d6bd1631a875
Nandhakumar <nandhu_kumar86@yahoo.com>
2021-04-05 23:32:01 +0530

    changes to pachk.json

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const path = require("path");
-const express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/angular-build'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cnnapp1'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/cnnapp1/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
